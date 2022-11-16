a8e56b6e6dcd3ca58b9c8f969e5992c494886c16
VladOniscenko <85153@glr.nl>
2022-11-16 21:36:36 +0100

    registratie form aangemaakt

--- registratie.php
+++ registratie.php
@@ -1,5 +1,8 @@
 <!-- Hier komt een registratie form. -->
-
+<?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,14 +12,16 @@
     <title>registratie</title>
 </head>
 <body>
+    <a href="./index.php">Terug naar inlog</a>
     <form action="./toDB/registrVerwerk.php" method="post">
 
-        <input type="text" name="username" required max="50" placeholder="Gebruikersnaam">
-        <input type="password" name="password" required max="30" placeholder="Wachtwoord">
-        <input type="password" name="passwordRepeat" required max="30" placeholder="Wachtwoord nogmaals">
-        <input type="text" name="Voornaam" required max="50" placeholder="Gebruikersnaam">
-        <input type="text" name="Achternaam" required max="50" placeholder="Gebruikersnaam">
-        <input type="date" name="Geboorte" required max="50" placeholder="Gebruikersnaam">
+        <input type="text" name="gebruikersnaam" required min="5" max="50" placeholder="Gebruikersnaam">
+        <input type="password" name="wachtwoord" required  min="5" max="50" placeholder="Wachtwoord">
+        <input type="password" name="wachtwoordRepeat" required  min="5" max="50" placeholder="Wachtwoord nogmaals">
+        <input type="text" name="voornaam" required min="3" max="50" placeholder="Voornaam">
+        <input type="text" name="achternaam" required min="3" max="50" placeholder="Achternaam">
+        <input type="date" name="geboorte" required placeholder="Geboortedatum">
+        <input type="email" name="email" required max="50" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="E-mail">
 
         <input type="submit" name="verzend">
 
