514c9b732fa75226b528914d48c140c71b6a1b90
Nicky <88220@glr.nl>
2022-11-21 22:35:34 +0100

    made login easier to navigate for screenreaders

--- index.php
+++ index.php
@@ -3,32 +3,47 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inlog</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    
-    <a href="index.php">Inlogen</a>
 
-    <form action="./toDB/inlogVerwerk.php" method="post">
+<body class="loginPage">
 
-        <?php
-            if(isset($_GET['result'])){
-                $res = $_GET['result'];
-                echo "<div>$res</div>";
-            }
+
+
+    <form action="./toDB/inlogVerwerk.php" method="post" class="loginContainer">
+        <fieldset>
+            <legend>
+                <h1>Inloggen</h1>
+            </legend>
+
+            <?php
+        if (isset($_GET['result'])) {
+            $res = $_GET['result'];
+            echo "<div>$res</div>";
+        }
         ?>
+            <label for="username">Username</label>
+            <input type="text" name="username" required max="50" placeholder="Username">
+            <label for="password">Password</label>
+            <input type="password" name="password" required max="30" placeholder="Password">
+            <div class="twoButtonContainer">
+                <a href="registratie.php">Registreren</a>
+                <input type="submit" name="verzend" value="inloggen">
+            </div>
 
-        <input type="text" name="username" required max="50" placeholder="Username">
-        <input type="password" name="password" required max="30" placeholder="Password">
-        <input type="submit" name="verzend">
-        
+        </fieldset>
+
+
     </form>
 
-    <a href="registratie.php">Een nieuwe account aanmaken</a>
+
 
 </body>
+
 </html>
