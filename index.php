cee2cbc12fafd267b1228854f5eed250f6813c03
Nicky <88220@glr.nl>
2022-11-23 20:00:22 +0100

    added styling to login and regristration

--- index.php
+++ index.php
@@ -9,41 +9,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inlog</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.sass">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-
-
-
-    <form action="./toDB/inlogVerwerk.php" method="post">
-        <fieldset>
-            <legend>
-                <h1>Inloggen</h1>
-            </legend>
-
+<div class="h-100 d-flex align-items-center justify-content-center bg-dark">
+    <div class="wrapper bg-light border-3">
+        <h1 class="p-4 mb-3">Inloggen</h1>
+        <form action="./toDB/inlogVerwerk.php" method="post" class="p-4">
             <?php
-        if (isset($_GET['message'])) {
-            $res = $_GET['message'];
-            echo "<div>$res</div>";
-        }
-        ?>
-            <label for="username">Username</label>
-            <input type="text" name="username" required max="50" placeholder="Username">
-            <label for="password">Password</label>
-            <input type="password" name="password" required max="30" placeholder="Password">
-            <div>
-                <a href="registratie.php">Registreren</a>
-                <input type="submit" name="verzend" value="inloggen"f>
+                if (isset($_GET['message'])) {
+                    $res = $_GET['message'];
+                    echo "<div>$res</div>";
+                }
+                ?>
+
+            <div class="mb-4">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" name="username" required max="50" placeholder="Username" class="form-control">
+            </div>    
+            <div class="mb-5">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" required max="30" placeholder="Password" class="form-control">
             </div>
 
-        </fieldset>
+            <div class="btn-toolbar justify-content-between">
+                <a href="registratie.php" class="btn btn-secondary">Registreren</a>
+                <input type="submit" name="verzend" value="inloggen" class="btn btn-primary">
+            </div>
 
 
-    </form>
+        </form>
+    </div>
+</div>
 
 
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- registratie.php
+++ registratie.php
@@ -1,52 +1,76 @@
 <!-- Hier komt een registratie form. -->
 <?php
-    error_reporting(E_ALL);
-    ini_set("display_errors", 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>registratie</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
-<body class="loginPage">
-    
-    <form action="./toDB/registrVerwerk.php" method="post" class="loginContainer">
-        <fieldset>
-            <legend><h1>Registreren</h1></legend>
 
-            <label for="gebruikersnaam">Gebruikersnaam</label>
-            <input type="text" name="gebruikersnaam" required min="5" max="50" placeholder="Gebruikersnaam">
-            
-            <label for="email">Email</label>
-            <input type="email" name="email" required max="50" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="E-mail">
-            
-            <label for="wachtwoord">Wachtwoord</label>
-            <input type="password" name="wachtwoord" required  min="5" max="50" placeholder="Wachtwoord">
-            
-            <label for="wachtwoordRepeat">Wachtwoord nogmaals</label>
-            <input type="password" name="wachtwoordRepeat" required  min="5" max="50" placeholder="Wachtwoord nogmaals">
-            
-            <label for="voornaam">Voornaam</label>
-            <input type="text" name="voornaam" required min="3" max="50" placeholder="Voornaam">
-            
-            <label for="achternaam">Achternaam</label>
-            <input type="text" name="achternaam" required min="3" max="50" placeholder="Achternaam">
-            
-            <label for="geboorte">Geboortedatum</label>
-            <input type="date" name="geboorte" required placeholder="Geboortedatum">
-            
-            <div class="twoButtonContainer">
-                <a href="./index.php">Terug naar inlog</a>
-                <input type="submit" name="verzend">
-            </div>
-        
-        </fieldset>
+<body>
+    <div class="h-100 d-flex align-items-center justify-content-center bg-dark">
+        <div class="wrapper bg-light">
+            <h1 class="p-4 mb-3">Registreren</h1>
+            <form action="./toDB/registrVerwerk.php" method="post" class="p-4">
+
+                <div class="mb-4">
+                    <label for="gebruikersnaam" class="form-label">Gebruikersnaam</label>
+                    <input type="text" name="gebruikersnaam" required min="5" max="50" placeholder="Gebruikersnaam" class="form-control">
+                </div>
+
+                <div class="mb-4">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" required max="50" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        placeholder="E-mail" class="form-control">
+                </div>
 
 
-    </form>
+                <div class="mb-4">
+                    <label for="wachtwoord" class="form-label">Wachtwoord</label>
+                    <input type="password" name="wachtwoord" required min="5" max="50" placeholder="Wachtwoord" class="form-control">
+                </div>
+
+                <div class="mb-4">
+                    <label for="wachtwoordRepeat" class="form-label">Wachtwoord nogmaals</label>
+                    <input type="password" name="wachtwoordRepeat" required min="5" max="50"
+                        placeholder="Wachtwoord nogmaals" class="form-control">
+                </div>
+
+                <div class="mb-4">
+                    <label for="voornaam" class="form-label">Voornaam</label>
+                    <input type="text" name="voornaam" required min="3" max="50" placeholder="Voornaam" class="form-control">
+                </div>
+
+                <div class="mb-4">
+                    <label for="achternaam" class="form-label">Achternaam</label>
+                    <input type="text" name="achternaam" required min="3" max="50" placeholder="Achternaam" class="form-control">
+                </div>
+
+                <div class="mb-5">
+                    <label for="geboorte" class="form-label">Geboortedatum</label>
+                    <input type="date" name="geboorte" required placeholder="Geboortedatum" class="form-control">
+                </div>
+
+
+                <div class="btn-toolbar justify-content-between">
+                    <a href="./index.php" class="btn btn-secondary">Terug naar inlog</a>
+                    <input type="submit" name="verzend" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
 </body>
+
 </html>
