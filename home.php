e596f11673e962b52de95ba52850adce3b14bfac
Nicky <88220@glr.nl>
2022-11-24 15:55:51 +0100

    a

--- home.php
+++ home.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Klasoverzicht - GLRtje</title>
     <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
     <!-- HEADER -->
@@ -41,13 +42,13 @@
 
             if(mysqli_num_rows($result) > 0){
                 // DIT IS WAT OP PAGINA KOMT
-                echo "<div class='studentContainer'>";
+                echo "<div class='row row-cols-1 row-cols-md-5 g-4'>";
                 while($item = mysqli_fetch_assoc($result)){
-                    echo "<div class='student'>";
-                    echo "<img class='avatar' src='./avatars/" . $item['avatar_leerling'] . "' width='100%'>";
-                    echo "<div class='naam'>" . $item['voornaam']." "."</div>";
-                    echo "<div class='achternaam'>". $item['achternaam']."</div>";
-                    echo "<a href='studentInformatie.php?id=".$item['leerlingnummer']."'>Info</a>";
+                    echo "<div class='card'>";
+                    echo "<img class='avatar card-img-top' src='./avatars/" . $item['avatar_leerling'] . "' width='100%'>";
+                    echo "<h5 class='naam card-title'>" . $item['voornaam']." "."</h5>";
+                    echo "<h6 class='achternaam card-subtitle'>". $item['achternaam']."</h6>";
+                    echo "<a href='studentInformatie.php?id=".$item['leerlingnummer']."' class='btn btn-primary'>Info</a>";
                     echo "</div>";
                 }
                 echo "</div>";
@@ -63,5 +64,6 @@
     <footer>
 
     </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
