f593013969828a9e6611e5f29a400bc1970867dc
Batuhan Bayraktar <87231@glr.nl>
2022-11-25 14:14:48 +0100

    overzicht van beoordelingen toegevoegd

--- studentInformatie.php
+++ studentInformatie.php
@@ -20,10 +20,14 @@ require_once './toDB/session.inc.php';
 //Maak de query
 $query = "SELECT * FROM tabel_leerlingen WHERE klas = '$klas' AND leerlingnummer = $id";
 $query2 = "SELECT * FROM tabel_groepen WHERE ID = $klas";
+$query3 = "SELECT * FROM `tabel_beoordelingen` WHERE`ID_leerling` = $id AND (`sleutelwoord_beoordeling` = 'Huiswerk gemaakt' OR `sleutelwoord_beoordeling` = 'Maaltijd opgegeten' OR `sleutelwoord_beoordeling` = 'Speelgoed opgeruimd' OR `sleutelwoord_beoordeling` = 'Goed gedragen' OR `sleutelwoord_beoordeling` = 'Iets anders positief');";
+$query4 = "SELECT * FROM `tabel_beoordelingen` WHERE `ID_leerling` = $id AND( `sleutelwoord_beoordeling` = 'Huiswerk niet gemaakt' OR `sleutelwoord_beoordeling` = 'Maaltijd niet opgegeten' OR `sleutelwoord_beoordeling` = 'Speelgoed niet opgeruimd' OR `sleutelwoord_beoordeling` = 'Niet goed gedragen' OR `sleutelwoord_beoordeling` = 'Iets anders negatief');";
 
 //Voer de query uit en vang het resultaat op
 $result = mysqli_query($mysqli,$query);
 $result2 = mysqli_query($mysqli,$query2);
+$result3 = mysqli_query($mysqli,$query3);
+$result4 = mysqli_query($mysqli,$query4);
 
 if(!$result){
     echo "<p>FOUT:</p>";
@@ -38,6 +42,14 @@ if(!$result2){
     echo "<p>" . mysqli_error($mysqli) . "</p>";
     exit;
 }
+
+if(!$result3){
+    echo "<p>FOUT:</p>";
+    echo "<p>" . $query . "</p>";
+    echo "<p>" . mysqli_error($mysqli) . "</p>";
+    exit;
+}
+
 
 if(mysqli_num_rows($result2) > 0){
 $item = mysqli_fetch_assoc($result2);
@@ -84,6 +96,39 @@ else
     echo "<p>Geen items gevonden!</p>";
     echo $klas ."<br>";
     echo $id . "<br>";
+}
+if(mysqli_num_rows($result3) > 0){
+    
+    echo "<h3>Positieve Beoordelingen</h3>";
+    echo "<table border='1px'><tr><th>Beschrijving beoordeling</th><th>Type beoordeling</th><th>Datum beoordeling</th></tr>";
+    while($item = mysqli_fetch_assoc($result3)){
+    echo "<tr>";
+
+    echo "<td>" .$item['beschrijving_beoordeling']."</td>";
+    echo "<td>" .$item['sleutelwoord_beoordeling']."</td>";
+    echo "<td>" .date('d-m-Y',strtotime($item['datum_beoordeling']))."</td>";
+    echo "<td><a href='aanpasBeoordeling.php?id=".$item['ID']."'>Aanpassen</a></td></tr>";    
+}
+    echo "</table><br><br>";
+}
+else{
+    echo "Er is geen positieve beoordeling gevonden in het systeem. <br>";
+}
+if(mysqli_num_rows($result4) > 0){
+    echo "<h3>Negatieve Beoordelingen</h3>";
+    echo "<table border='1px'><tr><th>Beschrijving beoordeling</th><th>Type beoordeling</th><th>Datum beoordeling</th></tr>";
+    while($item = mysqli_fetch_assoc($result4)){
+    echo "<tr>";
+
+    echo "<td>" .$item['beschrijving_beoordeling']."</td>";
+    echo "<td>" .$item['sleutelwoord_beoordeling']."</td>";
+    echo "<td>" .date('d-m-Y',strtotime($item['datum_beoordeling']))."</td>";
+    echo "<td><a href='aanpasBeoordeling.php?id=".$item['ID']."'>Aanpassen</a></td></tr>";
+    }
+    echo "</table>";
+}
+else{
+    echo "Er is geen negatieve beoordeling gevonden in het systeem. <br>";
 }
 
     ?>
